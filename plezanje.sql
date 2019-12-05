be0aff28331c6913dbc2b3519164935737763c54
spelaster <spela.ster97@gmail.com>
2019-12-05 09:44:54 +0100

    Update plezanje.sql

--- plezanje.sql
+++ plezanje.sql
@@ -3,80 +3,7 @@
 -- File generated with SQLiteStudio v3.2.1 on cet. nov. 28 09:46:17 2019
 --
 -- Text encoding used: System
---
-PRAGMA foreign_keys = off;
-BEGIN TRANSACTION;
 
--- Table: izlet
-CREATE TABLE izlet (
-    id         INTEGER PRIMARY KEY,
-    datum      DATE,
-    plezalisce INTEGER NOT NULL
-                       REFERENCES plezalisce (id) 
-);
-
-
--- Table: otrok
-CREATE TABLE otrok (
-    id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    ime           TEXT,
-    priimek       TEXT,
-    datum_rojstva DATE,
-    datum_vpisa   DATE
-);
-
-
--- Table: placilo
-CREATE TABLE placilo (
-    racun INTEGER PRIMARY KEY
-                  REFERENCES racun (id),
-    stars INTEGER REFERENCES stars (id),
-    datum DATE    NOT NULL
-);
-
-
--- Table: plezalisce
-CREATE TABLE plezalisce (
-    id  INTEGER PRIMARY KEY,
-    ime TEXT
-);
-
-
--- Table: racun
-CREATE TABLE racun (
-    otrok  INTEGER     REFERENCES otrok (id),
-    datum  DATENOT,
-    znesek DECIMAL (2),
-    izlet              REFERENCES izlet (id),
-    id     INTEGER     PRIMARY KEY
-                       NOT NULL,
-    UNIQUE (
-        otrok,
-        datum
-    )
-);
-
-
--- Table: skrbnik
-CREATE TABLE skrbnik (
-    otrok INTEGER REFERENCES otrok (id),
-    stars INTEGER REFERENCES stars (id),
-    PRIMARY KEY (
-        otrok,
-        stars
-    )
-);
-
-
--- Table: stars
-CREATE TABLE stars (
-    id      INTEGER PRIMARY KEY,
-    ime     TEXT,
-    priimek TEXT,
-    mail    TEXT,
-    naslov  TEXT,
-    telefon TEXT
-=======
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
@@ -104,6 +31,8 @@ CREATE TABLE stars (
 CREATE TABLE skrbnik (
     otrok INTEGER REFERENCES otrok(id),
     stars INTEGER REFERENCES stars(id),
+    dobiva_maile BOOLEAN,
+    UNIQUE(otrok, stars, dobiva_maile),
     PRIMARY KEY (otrok, stars)
 );
 
@@ -112,17 +41,11 @@ CREATE TABLE racun (
     otrok INTEGER REFERENCES otrok(id),
     datum DATENOT NULL,
     znesek DECIMAL(2),
+    datum_placila DATE,
     izlet REFERENCES izlet(id),
     id INTEGER PRIMARY KEY NOT NULL,
     UNIQUE (otrok, datum)
     
-);
-
--- Table: placilo
-CREATE TABLE placilo (
-    racun INTEGER PRIMARY KEY REFERENCES racun(id),
-    stars INTEGER REFERENCES stars(id),
-    datum DATE NOT NULL
 );
     
 -- Table: izlet
