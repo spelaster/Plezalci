080964b4dcf9dcc673bb0375929755bfa077b8d4
spelaster <spela.ster97@gmail.com>
2020-08-24 21:05:03 +0200

    Delete naredi_bazo.py

--- naredi_bazo.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-with open(preglednica_podatki) as dat:
-    for vrstica in csv.DictReader(dat):
-        conn.execute('INSERT ... ? ? ? ', [vrstica['Ime in priimek'], vrstica['telefon'], ...])
